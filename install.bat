64dfbc07f0edbd2291e4f15e0fa00491df1b9538
Fernando <fernandoairescastello@gmail.com>
2019-08-31 00:07:00 -0300

    Added the app icon

--- install.bat
+++ install.bat
@@ -1,2 +1,2 @@
-adb uninstall com.fernandoairescastello.magic_castle
-adb install C:\Users\fernando.aires\Proj\Fernando\MagicCastle\platforms\android\app\build\outputs\apk\debug\app-debug.apk
+adb uninstall com.fernandoairescastello.magiccastle
+adb install .\platforms\android\app\build\outputs\apk\debug\app-debug.apk
